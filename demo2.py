b60a0053fbb1602713aa6dbd4dfaae68ab02bd42
tongkuzk <160867542+tongkuzk@users.noreply.github.com>
2026-07-10 16:18:00 +0800

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -4,39 +4,44 @@ import csv
 
 #定义爬虫函数
 def crawl_boss_api():
-    #将操作模块实例化
+    #启动一个浏览器
     dp = ChromiumPage()
+    #目标链接
     base_url = 'https://www.zhipin.com/web/geek/job?query=Python爬虫&city=100010000'
 
     #创建csv文件
     with open('boss_jobs8.csv', 'w', encoding='utf-8', newline='') as f:
-        writer = csv.writer(f) #创建一个csv的编辑器
-        writer.writerow([#写入列表字段
+        writer = csv.writer(f) #对文件f创建一个csv的编辑器
+        writer.writerow([
             '岗位名称','公司','薪资','学历'
             ,'经验','技能','城市','地区'
-        ])
+        ])#写入列表字段
         #进行循环
         for page in range(35, 40):
             print(f'>>> 第 {page} 页')
 
             # 每页重新监听
-            dp.listen.clear()
-            dp.listen.start('wapi/zpgeek/search/joblist.json')
+            dp.listen.clear() 
+            #监听的接口
+            dp.listen.start('wapi/zpgeek/search/joblist.json') 
 
-            # 每页重新 get（Boss 翻页的本质）
+            # 每页重新 get
             dp.get(f'{base_url}&page={page}')
+            #等待页面加载
             dp.wait.load_start()
-
+            
+            #等待监听的接口，超时则返回未捕获到接口
             resp = dp.listen.wait(timeout=15)
             if not resp:
                 print('未捕获到接口')
                 break
-
+                
+            #数据的抓取，未抓取到则返回本业无数据
             job_list = resp.response.body.get('zpData', {}).get('jobList', [])
             if not job_list:
                 print('本页无数据')
                 break
-
+            
             for job in job_list:
                 writer.writerow([
                     job.get('jobName'),
